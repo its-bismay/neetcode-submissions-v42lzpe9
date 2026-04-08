1107f35f9506098557f808d3af9af8aa11efd286
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-04-08 19:26:18 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        n = len(position)
+        combined = list(zip(position, speed))
+        stk = []
+        for p, s in sorted(combined)[::-1]:
+            t = (target - p) / s
+            if not stk or t > stk[-1]:
+                stk.append(t)       
+        return len(stk)
