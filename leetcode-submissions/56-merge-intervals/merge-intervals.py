0fd26f35b731dcf47a99ebdb54659b68b206fc12
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-06-03 23:32:27 +0530

    Time: 11 ms (25.01%) | Memory: 23.2 MB (28.59%) - LeetSync

---
+++ leetcode-submissions/56-merge-intervals/merge-intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        ans=[intervals[0]]
+        for start, end in intervals[1:]:
+            lastEnd = ans[-1][1]
+
+            if lastEnd >= start:
+                ans[-1][1] = max(lastEnd, end)
+            else:
+                ans.append([start,end])
+        return ans 
+
+        
