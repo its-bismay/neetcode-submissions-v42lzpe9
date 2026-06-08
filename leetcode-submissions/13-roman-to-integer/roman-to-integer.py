47690a6ca3877f35f714457551e8ae2e5a74b3d5
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-06-08 22:58:59 +0530

    Time: 11 ms (14.95%) | Memory: 19.2 MB (88.95%) - LeetSync

---
+++ leetcode-submissions/13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanDict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        ans = 0
+        i = 0
+        while i < (len(s)):
+            if i < len(s) - 1 and romanDict[s[i]] < romanDict[s[i+1]]:
+                ans += (romanDict[s[i+1]] - romanDict[s[i]])
+                i += 2
+            else:
+                ans += romanDict[s[i]]
+                i += 1
+
+        
+        return ans
+
+        
