33f55eb2e7f81f5cce7fbe6eac27e538bcfe3358
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-07-14 19:38:24 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        visited = set()
+        added = set()
+        ans = []
+        hmap = defaultdict(list)
+
+        for course, prereq in prerequisites:
+            hmap[course].append(prereq)
+
+        def dfs(i):
+            if i in visited:
+                return False
+            elif i in added:
+                return True
+            
+            visited.add(i)
+            for preq in hmap[i]:
+                if not dfs(preq):
+                    return False
+
+            visited.remove(i)
+            added.add(i)
+            ans.append(i)
+            return True 
+
+
+
+        for i in range(numCourses):
+                if not dfs(i):
+                    return [] 
+        
+        return ans
+        
