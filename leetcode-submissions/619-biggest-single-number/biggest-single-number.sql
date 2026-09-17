a7b290e962f869ec0010dcbc1083982a718e0d54
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-09-17 23:06:39 +0530

    Time: 444 ms (76.12%) | Memory: 0B (100.00%) - LeetSync

--- leetcode-submissions/619-biggest-single-number/biggest-single-number.sql
+++ leetcode-submissions/619-biggest-single-number/biggest-single-number.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
 select max(num) as num
-from MyNumbers
-where num in (select num from MyNumbers group by num having count(num) = 1)
+from (select num from MyNumbers group by num having count(num) = 1) as new_table
