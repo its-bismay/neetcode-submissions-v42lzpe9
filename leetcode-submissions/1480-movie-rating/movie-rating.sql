4320c0a4b2c6c22de4d00bf92c1ece12c7fa010c
Bibhabasu Bismay Kumar Naik <2023ugee070@nitjsr.ac.in>
2026-09-25 15:26:09 +0530

    Time: 1428 ms (64.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode-submissions/1480-movie-rating/movie-rating.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+(select u.name as results
+from Users u
+left join MovieRating m
+on u.user_id = m.user_id
+group by u.user_id
+order by count(m.movie_id) desc, u.name
+limit 1)
+union all
+(select m.title as results
+from Movies m
+left join MovieRating mr
+on m.movie_id = mr.movie_id
+where extract(year_month from created_at) = 202002
+group by m.movie_id
+order by avg(mr.rating) desc, m.title
+limit 1)
